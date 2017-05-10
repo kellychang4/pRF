2d0002d00136014e9b065dec0e0e41a4492e6dba
Kelly Chang <kchang4@uw.edu>
2017-05-09 21:45:50 -0700

    update with retinotopy options

--- Code/CreateFunctions/createOpt.m
+++ Code/CreateFunctions/createOpt.m
@@ -14,7 +14,8 @@ function [opt] = createOpt(type)
 %
 % Outputs:
 %   opt
-%       map             Map name, string (i.e., 'Tonotopy') (default: '')
+%       map             Map name, string (i.e., 'Retinotopy', 'Tonotopy')
+%                       (default: '')
 %       model           Model name, also the function name to be fitted,
 %                       string (default: 'Gaussian1D')
 %       freeList        Free parameters to be estimated with opt.model,
@@ -41,7 +42,7 @@ function [opt] = createOpt(type)
 %       upSample        The desired up-sampling factor to create a new
 %                       resolution of the stimulus image (default: 1)
 %       parallel        Parallel processing (true) OR not (false), logical
-%                       (default: false)
+%                       (default: trues)
 %       quiet           Output progress in command window when fitting
 %                       (false) OR not (true), logical (default: false)
 %
@@ -65,14 +66,18 @@ flds = {'map', 'model', 'freeList', 'roi', 'estHRF', 'hrfThr', 'corrThr', ...
     'corr', 'CSS', 'cost', 'upSample', 'parallel', 'quiet'}; % field names
 
 switch lower(type)
+    case {'retinotopy', 'retina', 'ret', 'r'}
+        vals = {'''Retinotopy''', '''Gaussian2D''', ...
+            '{{''xMu'',''yMu'',''sigma''}}', '''''', 'NaN', '0.25', ...
+            '0.01', '''pearson''', 'false', 'struct()', '1', 'true', 'false'};
     case {'tonotopy', 'tono', 't'}
         vals = {'''Tonotopy''', '''Gaussian1D''', ...
             '{{''mu'',''0.01<sigma'',''0<exp<1''}}', '''''', 'NaN', '0.25', ...
-            '0.01', '''pearson''', 'true', 'struct()', '1', 'false', 'false'};
+            '0.01', '''pearson''', 'true', 'struct()', '1', 'true', 'false'};
     case {'default'}
         vals = {'''''', '''Gaussian1D''', '{{''mu'',''sigma''}}', ...
             '''''', 'NaN', '0.25', '0.01', '''pearson''', 'false', ...
-            'struct()', '1', 'false', 'false'};
+            'struct()', '1', 'true', 'false'};
 end
 
 evalStr = strcat('''', flds, '''', ',', vals);
